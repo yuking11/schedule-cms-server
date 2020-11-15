105ee0d30e3b34eb870fc8aef41caeb04301952f
yuking11 <ys11.works@gmail.com>
2020-11-16 00:07:06 +0900

    fix: add query year/month/day and swagger_auto_schema

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Schedule
 
-admin.site.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    """
+    docstring
+    """
+    list_display = ('event_date', 'category', 'title')
+    ordering = ('event_date',)
+    list_filter = ('event_date', 'category',)
+
+admin.site.register(Schedule, ScheduleAdmin)
--- schedule/views.py
+++ schedule/views.py
@@ -4,8 +4,11 @@ from .serializers import ScheduleSerializer
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import datetime
+from datetime import datetime, date, timedelta
 from dateutil.relativedelta import relativedelta
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class ScheduleView(generics.ListCreateAPIView):
 # class ScheduleView(APIView):
@@ -16,40 +19,68 @@ class ScheduleView(generics.ListCreateAPIView):
     def get_queryset(self):
         queryset = Schedule.objects.all()
 
-        limit = int(self.request.GET.get('limit'))
+        limit = self.request.GET.get('limit')
+        limit = int(limit) if limit is not None else limit
         year = self.request.GET.get('year')
+        year = int(year) if year is not None else year
         month = self.request.GET.get('month')
+        month = int(month) if month is not None else month
         day = self.request.GET.get('day')
+        day = int(day) if day is not None else day
 
-        today = datetime.date.today()
-
-        # if year is not None:
-        #     queryset = queryset.filter(event_date__year=year)
-        # else:
-        #     queryset = queryset.filter(event_date__year=today.year, event_date__month=today.month)
-
-        # if month is not None:
-        #     queryset = queryset.filter(event_date__month=month)
-
-        # if day is not None:
-        #     queryset = queryset.filter(event_date__day=day)
+        today = date.today()
 
         if limit is not None:
-            if year is not None:
-                start_date = datetime.datetime(int(year), 1, 1)
-                end_date = start_date + relativedelta(years=limit+1, days=-1)
-                queryset = queryset.filter(event_date__range=(start_date, end_date))
-            elif month is not None:
-                start_date = datetime.datetime(today.year, int(month), 1)
-                end_date = start_date + relativedelta(months=limit+1, days=-1)
-                queryset = queryset.filter(event_date__range=(start_date, end_date))
+            if year is not None and month is not None and day is not None:
+                start_date = datetime(year, month, day)
+                end_date = start_date + timedelta(days=limit-1)
+                queryset = queryset.filter(event_date__date__range=(start_date, end_date))
+            elif year is not None and month is not None:
+                start_date = datetime(year, month, 1)
+                end_date = start_date + relativedelta(months=limit, days=-1)
+                queryset = queryset.filter(event_date__date__range=(start_date, end_date))
             elif day is not None:
-                start_date = datetime.datetime(today.year, today.month, int(day))
-                end_date = start_date + relativedelta(days=limit)
-                queryset = queryset.filter(event_date__range=(start_date, end_date))
+                start_date = datetime(today.year, today.month, day)
+                end_date = start_date + timedelta(days=limit-1)
+                queryset = queryset.filter(event_date__date__range=(start_date, end_date))
+            elif month is not None:
+                start_date = datetime(today.year, month, 1)
+                end_date = start_date + relativedelta(months=limit, days=-1)
+                queryset = queryset.filter(event_date__date__range=(start_date, end_date))
+            elif year is not None:
+                start_date = datetime(year, 1, 1)
+                end_date = start_date + relativedelta(years=limit, days=-1)
+                queryset = queryset.filter(event_date__date__range=(start_date, end_date))
+        else:
+            if year is not None:
+                queryset = queryset.filter(event_date__year=year)
+            else:
+                queryset = queryset.filter(event_date__year=today.year)
+
+            if month is not None:
+                queryset = queryset.filter(event_date__month=month)
+
+            if day is not None:
+                queryset = queryset.filter(event_date__day=day)
 
         return queryset.order_by('event_date')
 
+    limit_param = openapi.Parameter(
+        'limit', openapi.IN_QUERY, description='取得件数', type=openapi.TYPE_NUMBER
+    )
+    year_param = openapi.Parameter(
+        'year', openapi.IN_QUERY, description='開催年', type=openapi.TYPE_NUMBER
+    )
+    month_param = openapi.Parameter(
+        'month', openapi.IN_QUERY, description='開催月', type=openapi.TYPE_NUMBER
+    )
+    day_param = openapi.Parameter(
+        'day', openapi.IN_QUERY, description='開催日付', type=openapi.TYPE_NUMBER
+    )
+
+    @swagger_auto_schema(manual_parameters=[limit_param, year_param, month_param, day_param])
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
 class ScheduleDetailView(generics.RetrieveUpdateDestroyAPIView):
     """スケジュール管理API"""
